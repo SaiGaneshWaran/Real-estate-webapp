8d1c1f4b27b501aedcad302bc5dec8f3deecdd79
Saiganeshwaran <saiganeshwaran@gmail.com>
2025-02-20 22:46:40 +0530

    12

---
+++ my-real-estate-app/src/lib/properties.js
@@ -0,0 +1,180 @@
+export const listData=[
+	
+        {
+            "id": "prop1",
+            "type": "House",
+            "bedrooms": 3,
+            "price": 750000,
+            "tenure": "Freehold",
+            size: 1800,
+            images: [
+                "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9WxGCT7If8NeyzkLXA74iWMnpllF-48A5ibA1hqwrsQYRb76UM-0B7EGneS6sUciedDM&usqp=CAU",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIV_usKWn7WbpGuQ4IQfmEKx9tNdvr9FJK3ARE_cHDwQSuLYBU5ZldQtMmdnxAUf4vUCY&usqp=CAU",
+                "https://www.thespruce.com/thmb/DN5_NsVfrhOXIJBI9n4CEFi9h58=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/3FraserLivingRoomFireplace-afa2735100754029985d3994bfd422f8.jpg",
+                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqIa_9DaZg4jLx32egmEmfBozYYDok2-uRQptOx465CtJK70RE6c5M-TVDz8yQ7oT2QbY&usqp=CAU"
+              ],
+            "description": "An attractive three-bedroom semi-detached family home located within a sought-after neighborhood, just 0.5 miles from Petts Wood station, offering fast and convenient train services for commuters. This beautifully maintained property features spacious living areas, a modern kitchen, and a well-kept garden, ideal for family gatherings or relaxation. Situated close to excellent local schools, shops, and amenities, it provides the perfect balance of comfort and convenience for a growing family. Don't miss the opportunity to own this charming home in a prime location. ",
+            "location": "Petts Wood Road, Petts Wood, Orpington BR5",
+            "picture": "https://lid.zoocdn.com/645/430/c2fbca62fc5d9d81b3db51a19340b53239d2e0b9.jpg",
+            "url": "properties/prop1.html",
+            "added": {
+                "month": "October",
+                "day": 12,
+                "year": 2022
+            },
+            "owner":"Adam Gilchrist"
+        },
+        {
+            "id": "prop2",
+            "type": "Flat",
+            "bedrooms": 2,
+            "price": 399995,size: 1200,
+            images: [
+                "https://images.pexels.com/photos/1918291/pexels-photo-1918291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                "https://images.pexels.com/photos/1428348/pexels-photo-1428348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                "https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                "https://images.pexels.com/photos/2467285/pexels-photo-2467285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                 "https://www.scaleinch.co.in/img-scaleinch/portfolio/1.jpg"
+              ],
+            "tenure": "Freehold",
+            "description": "Presented in excellent decorative order, this stunning two-bedroom, two-bathroom garden flat offers a perfect blend of style and functionality. Boasting modern amenities, it features a spacious open-plan living area, a contemporary kitchen with high-end appliances, and two well-appointed bathrooms for added convenience. The property also benefits from a private garden, ideal for outdoor relaxation or entertaining. Located in a desirable area, it is close to local shops, schools, and transport links, making it a perfect choice for professionals or small families seeking comfort and connectivity.",
+            "location": "Crofton Road Orpington BR6",
+            "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAomgUQlwECyvRqiSCw2ZPzw4HMxiMMlH8_Q&s",
+            "url": "properties/prop2.html",
+            "added": {
+                "month": "September",
+                "day": 14,
+                "year": 2022
+            },
+            "owner":"Alex Morgan"
+        },
+        {
+            "id": "prop3",
+            "type": "Bungalow",
+            "bedrooms": 4,
+            "price": 525000,size: 2000,
+            images: [
+                "https://www.dream-plans.com/en/GB/userfiles/products/BUNGALOW%20204/b204_f1zo3.jpg",
+                "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/44/38/ff/kandy-hill-bungalow.jpg?w=700&h=-1&s=1",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStT3tYLAhSi06DgBS7GDcuMHQT0P8Cf4KdsA&s",
+                "https://cf.bstatic.com/xdata/images/hotel/max1024x768/436237519.jpg?k=c45253ab37488925372c635e069d42315c6336edf2fa2379e999bcc97305063c&o=&hp=1",
+                 "https://q-xx.bstatic.com/xdata/images/hotel/max500/484217944.jpg?k=6dfe6a1e431ae1942ba669c8b25f3a3eda99bfa430a6d48d4ceabe727ddb1479&o="
+              ],
+            "tenure": "Leasehold",
+            "description": "Discover this spacious four-bedroom detached bungalow, designed to offer both comfort and modern living. Featuring a bright and airy open-plan living area, this home is perfect for entertaining and family gatherings. The property boasts a large, well-maintained garden, ideal for outdoor activities or serene relaxation, and a private garage providing ample storage or secure parking. Situated in a peaceful neighborhood, this bungalow offers easy access to local amenities, schools, and transport links, making it an excellent choice for those seeking a blend of convenience and tranquility.",
+            "location": "Meadow Lane, Bromley BR7",
+            "picture": "https://www.dream-plans.com/en/GB/userfiles/products/BUNGALOW%20204/b204_f1zo3.jpg",
+            "url": "properties/prop3.html",
+            "added": {
+                "month": "August",
+                "day": 8,
+                "year": 2023
+            },
+            "owner":"Taylor Reed"
+        },
+        {
+            "id": "prop4",
+            "type": "Studio",
+            "bedrooms": 1,
+            "price": 220000,size: 500,
+            images: [
+                "https://pix10.agoda.net/hotelImages/6474762/0/410f8a5f6c02ae65a44d75c1655eb679.jpg?ca=7&ce=1&s=414x232&ar=16x9",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQzaGKLH7ew8Bu2svnObGd60LJZdIDGrKWw&s",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyBi-2Jam70SL8RSUC8nNPQfBhkRDTEHfcOJcXMuQZk275ntmd8W6GX8pNV0fV1mYvkNw&usqp=CAU",
+                "https://host-students.com/app/uploads/2023/10/Host-The-Hub-Vauxhall-London-1320x566-premium-3.jpg",
+                 "https://media.karousell.com/media/photos/products/2021/11/12/247_selfservice_recording_stud_1636737055_764c48cb.jpg"
+              ],
+            "tenure": "Leasehold",
+            "description": "This modern studio apartment is perfectly situated in the heart of the city, offering unparalleled convenience and contemporary living. The property boasts a fully equipped kitchen with state-of-the-art appliances, making meal preparation effortless. The thoughtfully designed layout maximizes space, creating a comfortable and stylish living area ideal for professionals or students. With its prime location, you'll enjoy easy access to shopping, dining, entertainment, and excellent transport links, ensuring you're always connected to the vibrant city lifestyle.",
+            "location": "High Street, London EC1",
+            "picture": "https://pix10.agoda.net/hotelImages/6474762/0/410f8a5f6c02ae65a44d75c1655eb679.jpg?ca=7&ce=1&s=414x232&ar=16x9",
+            "url": "properties/prop4.html",
+            "added": {
+                "month": "June",
+                "day": 15,
+                "year": 2023
+            },
+            "owner":"Jordan Harper"
+        },
+        {
+            "id": "prop5",
+            "type": "House",
+            "bedrooms": 5,
+            "price": 1250000,size: 3000,
+            images: [
+                "https://media.rightmove.co.uk/dir/56k/55396/148599950/55396_2291218_IMG_00_0000_max_656x437.jpeg",
+                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIV_usKWn7WbpGuQ4IQfmEKx9tNdvr9FJK3ARE_cHDwQSuLYBU5ZldQtMmdnxAUf4vUCY&usqp=CAU",
+                "https://www.thespruce.com/thmb/DN5_NsVfrhOXIJBI9n4CEFi9h58=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/3FraserLivingRoomFireplace-afa2735100754029985d3994bfd422f8.jpg",
+                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqIa_9DaZg4jLx32egmEmfBozYYDok2-uRQptOx465CtJK70RE6c5M-TVDz8yQ7oT2QbY&usqp=CAU",
+                "https://cf.bstatic.com/xdata/images/hotel/max1024x768/436237519.jpg?k=c45253ab37488925372c635e069d42315c6336edf2fa2379e999bcc97305063c&o=&hp=1"
+              ],
+            "tenure": "Freehold",
+            "description": "Experience unparalleled luxury in this exquisite five-bedroom detached house, meticulously designed to combine elegance and functionality. The property features a private driveway offering ample parking, a beautifully landscaped spacious garden perfect for entertaining or relaxation, and premium interiors crafted with the finest materials. From the grand entrance hall to the expansive living areas and state-of-the-art kitchen, every detail reflects sophistication. Located in a prestigious neighborhood, this home provides easy access to top-rated schools, shopping, and transport links, making it the ultimate choice for modern family living.",
+            "location": "Oakwood Avenue, Beckenham BR3",
+            "picture": "https://media.rightmove.co.uk/dir/56k/55396/148599950/55396_2291218_IMG_00_0000_max_656x437.jpeg",
+            "url": "properties/prop5.html",
+            "added": {
+                "month": "July",
+                "day": 20,
+                "year": 2023
+            },
+            "owner":"Casey Blake"
+        },
+        {
+            "id": "prop6",
+            "type": "Flat",
+            "bedrooms": 3,
+            "price": 650000,size: 1400,
+            images: [
+                "https://cf.bstatic.com/xdata/images/hotel/max1024x768/491037212.jpg?k=9dc1e98ae201c3d9f867a9a7f5636ffb05e27e93ca7a42c6c8d0b7650f0c0cab&o=&hp=1",
+                "https://images.pexels.com/photos/1428348/pexels-photo-1428348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQzaGKLH7ew8Bu2svnObGd60LJZdIDGrKWw&s",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyBi-2Jam70SL8RSUC8nNPQfBhkRDTEHfcOJcXMuQZk275ntmd8W6GX8pNV0fV1mYvkNw&usqp=CAU",
+                
+                 "https://www.scaleinch.co.in/img-scaleinch/portfolio/1.jpg"
+              ],
+            "tenure": "Freehold",
+            "description": "Discover the epitome of modern living in this beautiful three-bedroom flat, offering breathtaking river views that provide a sense of tranquility and luxury. The property features an open-plan kitchen equipped with high-end appliances, seamlessly flowing into a spacious living area designed for both comfort and entertaining. Each bedroom is thoughtfully laid out to maximize space and natural light. Situated in a prime location, this flat offers easy access to local amenities, transport links, and vibrant community hubs, making it an ideal choice for families and professionals seeking a refined urban lifestyle",
+            "location": "Riverside Drive, Greenwich SE10",
+            "picture": "https://cf.bstatic.com/xdata/images/hotel/max1024x768/491037212.jpg?k=9dc1e98ae201c3d9f867a9a7f5636ffb05e27e93ca7a42c6c8d0b7650f0c0cab&o=&hp=1",
+            "url": "properties/prop6.html",
+            "added": {
+                "month": "May",
+                "day": 5,
+                "year": 2023
+            },
+            "owner":"Riley Quinn"
+        },
+        {
+            "id": "prop7",
+            "type": "Bungalow",size: 1600,
+            "bedrooms": 3,
+            "price": 475000,
+            images: [
+                "https://homestra.com/cdn-cgi/image/width=1920,quality=80/https://storage.googleapis.com/homestra-images/property-image-4ca436b6-7068-4c65-aeec-6ce680fc4878-1707985018.jpg",
+                "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/44/38/ff/kandy-hill-bungalow.jpg?w=700&h=-1&s=1",
+                "https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                "https://cf.bstatic.com/xdata/images/hotel/max1024x768/436237519.jpg?k=c45253ab37488925372c635e069d42315c6336edf2fa2379e999bcc97305063c&o=&hp=1",
+                 "https://media.karousell.com/media/photos/products/2021/11/12/247_selfservice_recording_stud_1636737055_764c48cb.jpg"
+              ],
+            "tenure": "Leasehold",
+            "description": "Step into this charming three-bedroom maisonette, thoughtfully spread over two floors to provide a harmonious blend of timeless elegance and modern living. Showcasing delightful period features such as high ceilings, intricate moldings, and large sash windows, this home retains its classic character while offering modern comforts. The spacious living areas are complemented by a contemporary kitchen and well-appointed bedrooms, providing both style and functionality. Located in a desirable area, this maisonette is close to local amenities, schools, and excellent transport links, making it a perfect choice for families or professionals looking for a distinctive and comfortable home. ",
+            "location": "Elm Grove, Orpington BR6",
+            "picture": "https://homestra.com/cdn-cgi/image/width=1920,quality=80/https://storage.googleapis.com/homestra-images/property-image-4ca436b6-7068-4c65-aeec-6ce680fc4878-1707985018.jpg",
+            "url": "properties/prop7.html",
+            "added": {
+              "month": "November",
+              "day": 3,
+              "year": 2023
+            },
+            "owner":"Morgan Ellis"
+          }
+]
+
+  export const userData = {
+    id: 1,
+    name: "Saiganeshwaran",
+    image: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  };
+
+    
